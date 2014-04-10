94909c65610eef4a1f1f5d82a846722ef3658dac
Filipe Rodrigues <subrodrigues@gmail.com>
2014-04-10 18:12:08 +0100

    DT added

--- TAS_JavaAndScala/src/scalav/SProgram.scala
+++ TAS_JavaAndScala/src/scalav/SProgram.scala
@@ -1,11 +1,11 @@
 package scalav
 
-class SProgram(fileName: String) {	
+class SProgram(fileName: String, dT : Int) {	
 	val lines = io.Source.fromFile(fileName).getLines.toList
-
+	val dt = dT
+	
 	def execute(index: Int) {
 		println(lines(index) + " from Scala")
-
 //		println("Sleep 800ms\n");
 //		Thread.sleep(500);
 	}
