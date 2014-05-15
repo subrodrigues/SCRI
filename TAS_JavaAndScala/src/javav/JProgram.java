af2a6d064cb2e173cf1b7ed289ede098035ca711
Pedro Costa <pedro.vendascosta@gmail.com>
2014-05-15 23:14:12 +0100

    Java variant working.

--- TAS_JavaAndScala/src/javav/JProgram.java
+++ TAS_JavaAndScala/src/javav/JProgram.java
@@ -7,15 +7,25 @@ import java.nio.file.Paths;
 import java.util.Date;
 import java.util.List;
 
+import jp.vdmtools.VDM.CGException;
+
 public class JProgram implements Runnable {
+	public static double ERROR = -1;
+	public static double EOF = -2;
 	List<String> lines = null;
 	private long dt = 0;
 	private int index;
 	private double result;
+	private ATInput input;
 
 	public JProgram(String fileName, long dt, int index) {
 		this.dt = dt;
 		this.index = index;
+		try {
+			this.input = new ATInput();
+		} catch (CGException e1) {
+			e1.printStackTrace();
+		}
 
 		// Get all the lines
 		try {
@@ -26,26 +36,107 @@ public class JProgram implements Runnable {
 		}
 	}
 
-	public double getResult(){
+	private Line readLine() {
+		String line = lines.get(index);
+		String sensors = line.split("#")[1];
+		String sensor1 = sensors.split(":")[0];
+		String sensor2 = sensors.split(":")[1];
+		String sensor3 = sensors.split(":")[2];
+
+		return new Line(toSensor(sensor1), toSensor(sensor2), toSensor(sensor3),index);
+	}
+
+	private Sensor toSensor(String sensor) {
+		String[] vals = sensor.split(" ");
+		double pe = readValue(vals[1]);
+		double pd = readValue(vals[2]);
+		double t = readValue(vals[3]);
+
+		return new Sensor(pe, pd, t);
+	}
+
+	private double readValue(String vals) {
+		double value;
+		try{
+			value= Double.parseDouble(vals);
+		}catch(NumberFormatException e){
+			value = ERROR;
+		}
+		
+		return value;
+	}
+
+	public double getResult() {
 		return result;
 	}
-	
+
 	@Override
 	public void run() {
 		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			Line line = readLine();
+			result = input.read(line.s1.pe, line.s1.pd, line.s1.t,
+					line.s2.pe, line.s2.pd, line.s2.t, line.s3.pe, line.s3.pd,
+					line.s3.t).doubleValue();
+			System.out.println(line.toString());
+			index++;
+		} catch (CGException e) {
 			e.printStackTrace();
+		} catch(IndexOutOfBoundsException e){
+			result = EOF;
 		}
-		System.out.println(lines.get(index) + " from Java");
+	}
 
-		// try {
-		// System.out.println("Sleep 800ms\n");
-		// Thread.sleep(500);
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
+	private class Sensor {
+		private double pe, pd, t;
+
+		public Sensor(double pe, double pd, double t) {
+			this.pe = pe;
+			this.pd = pd;
+			this.t = t;
+		}
+		
+		@Override
+		public String toString(){
+			String ret="";
+			
+			if( pe != ERROR )
+				ret+=""+pe+" ";
+			else
+				ret+="-- ";
+			
+			if( pd != ERROR )
+				ret+=""+pd+" ";
+			else
+				ret+="-- ";
+			
+			if( t != ERROR )
+				ret+=""+t;
+			else
+				ret+="--";
+			
+			return ret;
+		}
+	}
+
+	private class Line {
+		private Sensor s1, s2, s3;
+		private int index;
+
+		public Line(Sensor s1, Sensor s2, Sensor s3, int index) {
+			this.s1 = s1;
+			this.s2 = s2;
+			this.s3 = s3;
+			this.index = index;
+		}
+		
+		@Override
+		public String toString(){
+			String ret = ""+(index+1) + "# ";
+			ret+=s1.toString() + " : ";
+			ret+=s2.toString() + " : ";
+			ret+=s3.toString();
+			return ret;
+		}
 	}
 
 }
--- TAS_JavaAndScala/src/main/MainProgram.java
+++ TAS_JavaAndScala/src/main/MainProgram.java
@@ -1,109 +1,109 @@
 package main;
 
 import java.util.Date;
-
 import javav.JProgram;
-import scalav.SProgram;
+//import scalav.SProgram;
 
 public class MainProgram {
-	
+
 	JProgram jProgram = null;
-	SProgram sProgram = null;
-	
+	// SProgram sProgram = null;
+
 	private static final int MIN_DT = 200;
-	
+
 	private long dt;
-	
-	public MainProgram(long dt){
-		jProgram = new JProgram("src/files/input.txt", dt,0);
-		sProgram = new SProgram("src/files/input.txt", dt,0);
+
+	public MainProgram(long dt) {
+		
+		jProgram = new JProgram("src/files/input.txt", dt, 0);
 		this.dt = dt;
 	}
-	
+
 	public void printMessage() {
 		System.out.println("TAS - True AirSpeed\n");
 	}
 
 	public static void main(final String... arguments) {
+
+		System.out.println("Hello");
 		
-		if(arguments.length < 1){
+		if (arguments.length < 1) {
 			System.err.println("You should specify the DT!");
 			return;
 		}
-		
+
 		long dt;
-		try{
+		try {
 			dt = Long.parseLong(arguments[0]);
-		}catch(NumberFormatException e){
+		} catch (NumberFormatException e) {
 			System.err.println("DT must be a long number!");
 			return;
 		}
-		
-		if(dt <MIN_DT){
+
+		if (dt < MIN_DT) {
 			System.err.println("DT must b at least " + MIN_DT + "ms");
 			return;
 		}
-		
+
 		MainProgram main = new MainProgram(dt);
 		main.printMessage();
-		
-		for(int i = 0; i < 2; i++){
-			main.readLine(i);
-			
+
+		for (int i = 0; i < 2; i++) {
+			//main.readLine(i);
+			main.jProgram.run();
 		}
 	}
-	
+
 	public void readLine(int index) {
-		
-	    synchronized(this) {
-	        Thread t1 = new Thread(jProgram);
-	        Thread t2 = new Thread(sProgram);
-	        
-	        t1.start();
-	        t2.start();
-	        
-	        long startTime = System.currentTimeMillis();
-	        try {
+
+		synchronized (this) {
+			Thread t1 = new Thread(jProgram);
+			Thread t2 = new Thread(jProgram);
+
+			t1.start();
+			t2.start();
+
+			long startTime = System.currentTimeMillis();
+			try {
 				t1.join(dt);
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
-	        long endTime = System.currentTimeMillis();
-	        long elapsedTime = endTime-startTime;
-	        
-	        //TODO Check if dt-elapsedTime is 0
-	        if(dt-elapsedTime <= 0){
-	        	t1.stop();
-	        }
-	        else{
-	        	//TODO get result from t1
-	        }
-	        
-	        try {
-	        	if( dt-elapsedTime > 0)
-	        		t2.join(dt-elapsedTime);
-	        	else if(t2.isAlive()){
-	        		//ERROR
-	        	}
-	        	else{
-	        		//TODO get result from t2
-	        	}
+			long endTime = System.currentTimeMillis();
+			long elapsedTime = endTime - startTime;
+
+			// TODO Check if dt-elapsedTime is 0
+			if (dt - elapsedTime <= 0) {
+				t1.stop();
+			} else {
+				// TODO get result from t1
+			}
+
+			try {
+				if (dt - elapsedTime > 0)
+					t2.join(dt - elapsedTime);
+				else if (t2.isAlive()) {
+					// ERROR
+				} else {
+					// TODO get result from t2
+				}
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
-	        
-	        long finalTime = System.currentTimeMillis();
-	        long finalElapsedTime = finalTime-startTime;
-	     
+
+			long finalTime = System.currentTimeMillis();
+			long finalElapsedTime = finalTime - startTime;
+
 			try {
-				if(dt-finalElapsedTime > 0){
-					System.out.println("Sleep " + (dt-finalElapsedTime) + "ms\n");
-					Thread.sleep(dt-finalElapsedTime);
+				if (dt - finalElapsedTime > 0) {
+					System.out.println("Sleep " + (dt - finalElapsedTime)
+							+ "ms\n");
+					Thread.sleep(dt - finalElapsedTime);
 				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-	    }
+		}
 	}
-	
+
 }
